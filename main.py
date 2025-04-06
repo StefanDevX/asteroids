5e7edf3c158a96cca81a6c61e9f47b5465db9203
Stefan Weisbrodt <weisbrodt.stefan@gmail.com>
2025-04-06 19:54:37 +0100

    Asteroids - ALL DONE

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import sys
 import time
 from constants import *
 from player import Player
+from asteroidfield import *
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,13 +15,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     dt = 0
     
@@ -31,6 +39,17 @@ def main():
         screen.fill("black")
     
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collide(player):
+                print("Game over!")
+                sys.exit()
+        
+            for shot in shots:
+                if asteroid.collide(shot):
+                    shot.kill()
+                    asteroid.split()
+                  
 
         for obj in drawable:
             obj.draw(screen)
